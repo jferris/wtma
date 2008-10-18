f26bfd8f4e69ea5758bd1162df15b9920465a7a4
Mike Burns <mburns@thoughtbot.com>
2008-10-17 21:34:47 -0400

    OpenID

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,8 @@ Rails::Initializer.run do |config|
              :lib => 'clearance',
              :source => 'http://gems.github.com/'
 
+  config.gem "ruby-openid", :lib => 'openid'
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
