9969ef9ba4b24c633fcbd71681dc1ad61dfd4bae
Chad Pytel <cpytel@thoughtbot.com>
2008-10-17 23:47:32 -0400

    i can has gems?

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,7 @@ Rails::Initializer.run do |config|
              :lib => 'clearance',
              :source => 'http://gems.github.com/'
 
-  config.gem "ruby-openid", :lib => 'openid'
+  config.gem "ruby-openid", :lib => 'openid', :version => "~> 2.1.2"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
