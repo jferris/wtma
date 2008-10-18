7cb67f2f90f6f71a2606fce5dceeff436a2029c9
Mike Burns <mburns@thoughtbot.com>
2008-10-18 14:01:46 -0400

    Put cheap Items on the home page map

--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -16,6 +16,13 @@ class ItemsControllerTest < ActionController::TestCase
       should_assign_to :purchases
       should_assign_to :map
 
+      should "set @purchases to the cheapest purchase for that item" do
+        assigns(:purchases).each do |purchase|
+          item = purchase.item
+          assert_equal item.cheapest_purchase, purchase
+        end
+      end
+
       should "initialize @map" do
         # Looking at init_begin modifies it.
         assert_not_nil assigns(:map).send(:instance_variable_get,'@init_begin').first
@@ -24,13 +31,6 @@ class ItemsControllerTest < ActionController::TestCase
       should "center the map on the USA" do
         assert_match /map.setCenter\(new GLatLng\(#{USA[0]},#{USA[1]}\),#{DEFAULT_ZOOM_LEVEL}\)/,
                      assigns(:map).send(:instance_variable_get, '@init_begin').first.variable
-      end
-
-      should "set @purchases to the cheapest purchase for that item" do
-        assigns(:purchases).each do |purchase|
-          item = purchase.item
-          assert_equal item.cheapest_purchase, purchase
-        end
       end
 
       should "output the GMap headers" do
@@ -43,6 +43,16 @@ class ItemsControllerTest < ActionController::TestCase
 
       should "have the div for the map" do
         assert_select 'div[id=?]', assigns(:map).container
+      end
+
+      should "not put the map initialization in an onload" do
+        assert_no_match /window.onload = addCodeToFunction\(window.onload,function/, @response.body
+      end
+
+      should "put the cheapest Purchase for an Item on the map" do
+        assigns(:purchases).each do |purchase|
+          assert_match /GLatLng\(#{purchase.store.latitude},#{purchase.store.longitude}\)/, @response.body
+        end
       end
     end
   end
