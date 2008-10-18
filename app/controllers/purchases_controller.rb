acf9d444ae434722c781c3385062f2c1c81b8155
Joe Ferris <jferris@thoughtbot.com>
2008-10-18 13:41:29 -0400

    Added the new purchase form

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,16 @@ class PurchasesController < ApplicationController
 
   def index
     @purchases = current_user.purchases.latest
+    @new_purchase = Purchase.new
+  end
+
+  def create
+    @purchase = current_user.purchases.build(params[:purchase])
+    if @purchase.save
+      @new_purchase = Purchase.new
+    else
+      @new_purchase = @purchase
+    end
   end
 
 end
