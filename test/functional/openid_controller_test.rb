aae5e52682364a89c7a1a0a6d06972ba8b3ecebc
Joe Ferris <jferris@thoughtbot.com>
2008-10-18 14:23:33 -0400

    Fixed the openid authentication tests (full coverage, no external services used)

--- test/functional/openid_controller_test.rb
+++ test/functional/openid_controller_test.rb
@@ -5,30 +5,42 @@ class OpenidControllerTest < ActionController::TestCase
     @openid_identity = 'http://example.com/'
   end
 
-  context "POST to create" do
-    setup do
-      @location = 'Boston, MA'
-      post :create, {:openid_identifier => @openid_identity, :user => {:location => @location}}
-    end
-
-    before_should "not verify authenticity" do
-      @controller.expects(:verify_authenticity_token).never
-    end
-
-    should "stash the location in the session" do
-      assert_equal @location, session[:location]
+  def self.should_authenticate_with_openid
+    before_should "attempt to authenticate with openid" do
+      @controller.
+        expects(:authenticate_with_open_id).
+        with(nil, :optional => [:nickname, :postcode]).
+        yields(@result, @openid_identity, @registration)
     end
   end
 
-  context "successful login" do
+  context "when authenticating with openid would succeed" do
     setup do
-      result = stub('successful_result',:successful? => true)
+      @result = stub('successful_result',:successful? => true)
       @registration = { 'nickname' => 'Francis', 'postcode' => '60647' }
-      @controller.stubs(:authenticate_with_open_id).with(nil, :optional => [:nickname, :postcode]).
-        yields(result, @openid_identity, @registration)
+      @controller.
+        stubs(:authenticate_with_open_id).
+        yields(@result, @openid_identity, @registration)
     end
 
-    context "a new user" do
+    context "POST to create" do
+      setup do
+        @location = 'Boston, MA'
+        post :create, {:openid_identifier => @openid_identity, :user => {:location => @location}}
+      end
+
+      before_should "not verify authenticity" do
+        @controller.expects(:verify_authenticity_token).never
+      end
+
+      should "stash the location in the session" do
+        assert_equal @location, session[:location]
+      end
+
+      should_authenticate_with_openid
+    end
+
+    context "authenticating as a first-time user" do
       setup do
         User.delete_all(:openid_identity => @openid_identity)
         @location = "41 Winter St. Boston, MA 02108"
@@ -58,9 +70,11 @@ class OpenidControllerTest < ActionController::TestCase
       should "log the user in" do
         assert_equal @user.id, session[:user_id]
       end      
+
+      should_authenticate_with_openid
     end
 
-    context "an existing user" do
+    context "authenticating as a pre-existing user" do
       setup do
         Factory(:user, :openid_identity => @openid_identity)
         get :create, :openid_identifier => @openid_identity
@@ -72,6 +86,31 @@ class OpenidControllerTest < ActionController::TestCase
       should "log the user in" do
         assert_equal @user.id, session[:user_id]
       end
+
+      should_authenticate_with_openid
+    end
+  end
+
+  context "when authenticating with openid would fail" do
+    setup do
+      @message = 'no good'
+      @result = stub('successful_result', :successful? => false,
+                                          :message     => @message)
+      @registration = { 'nickname' => 'Francis', 'postcode' => '60647' }
+      @controller.
+        stubs(:authenticate_with_open_id).
+        yields(@result, @openid_identity, @registration)
+    end
+
+    context "authenticating" do
+      setup do
+        get :create, :openid_identifier => @openid_identity
+      end
+
+      should_set_the_flash_to 'no good'
+      should_redirect_to 'new_session_url'
+
+      should_authenticate_with_openid
     end
   end
 end
