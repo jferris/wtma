674a61cfada51dfe7acabceff34667fee2612490
Chad Pytel <cpytel@thoughtbot.com>
2008-10-17 23:15:54 -0400

    test for default name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,4 +2,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
   include Clearance::Test::Unit::UserTest
+  
+  context "a user" do
+    setup { @user = Factory(:user) }
+    
+    should "have the default first name of Joe" do
+      assert_equal "Joe", @user.first_name
+    end
+  end
 end
