ecc2d6eb1cfe20e045a402226e9f46162734a1ab
Mike Burns <mburns@thoughtbot.com>
2008-10-17 23:10:57 -0400

    OpenID

--- app/controllers/openid_controller.rb
+++ app/controllers/openid_controller.rb
@@ -1,5 +1,18 @@
 class OpenidController < ApplicationController
   def create
-    redirect_to new_purchase_path
+    authenticate_with_open_id do |result, openid_identity, registration|
+      if result.successful?
+        user = User.find_or_create_by_openid(openid_identity, registration)
+        if user.save
+          session[:user_id] = user.id
+          redirect_to new_purchase_path
+        else
+          redirect_to new_session_path
+        end 
+      else
+        flash[:warning] = result.message
+        redirect_to new_session_path
+      end 
+    end 
   end
 end
