69569ea3208ecac24efc3f9af860ea85264d3cf1
Chad Pytel <cpytel@thoughtbot.com>
2008-10-19 11:58:56 -0400

    integration of views for user editing enable changing of password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
   include Clearance::App::Controllers::UsersController
+  
+  before_filter :authenticate, :except => [:new, :create]
+  before_filter :ensure_owner, :except => [:new, :create]
+  
+  def edit
+    @user = User.find params[:id]
+  end
+  
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(params[:user])
+      flash[:info] = "Your account was successfully updated."
+      redirect_to edit_user_path(@user)
+    else
+      render :action => :edit
+    end
+  end
+  
+  protected
+  
+  def ensure_owner
+    unless current_user == User.find(params[:id])
+      deny_access
+    end
+  end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -15,4 +15,101 @@ class UsersControllerTest < ActionController::TestCase
       should_have_map_observer map
     end
   end
+  
+  public_context do
+    context "with a user" do
+      setup do
+        @user = Factory(:user)
+      end
+      should_deny_access_on "get :edit, :id => @user"
+    end
+  end
+  
+  logged_in_user_context do
+    context "with another user" do
+      setup do
+        @another_user = Factory(:user)
+      end
+      should_deny_access_on "get :edit, :id => @another_user"
+      should_deny_access_on "put :update, :id => @another_user, :user => {}"
+    end
+  end
+  
+  logged_in_user_context do
+    context "on GET to edit" do
+      setup do
+        get :edit, :id => @user
+      end
+    
+      should_assign_to :user
+      should_have_map :user
+      
+      should "display the email field" do
+        assert_select "input#user_email"
+      end
+      
+      should "display the change password fields" do
+        assert_select "input#user_password"
+        assert_select "input#user_password_confirmation"
+      end
+      
+      should "not display the openid field" do
+        assert_select "input#openid_identity", false
+      end
+    end
+    
+    context "when the user is an openid user" do
+      setup do
+        @user.update_attributes!({:openid_identity => "openid.openid_identity.com",
+                                  :email => nil})
+      end
+      
+      context "on GET to edit" do
+        setup do
+          get :edit, :id => @user
+        end
+      
+        should "display the openid_identity field" do
+          assert_select "input#user_openid_identity"
+        end
+      
+        should "not display the email field" do
+          assert_select "input#user_email", false
+        end
+      
+        should "not display the password fields" do
+          assert_select "input#password", false
+          assert_select "input#password_confirmation", false
+        end
+      end
+    end
+    
+    context "on PUT to update with valid params" do
+      setup do
+        put :update, :id => @user, :user => { :first_name => "Valid Name" }
+        @user.reload
+      end
+      
+      should_assign_to :user
+      should_set_the_flash_to /successfully/
+      should_redirect_to "edit_user_path(@user)"
+      should "update the user" do
+        assert_equal "Valid Name", @user.first_name
+      end
+    end
+    
+    context "on PUT to update wuth invalid params" do
+      setup do
+        put :update, :id => @user, :user => { :location => "" }
+      end
+      
+      should_assign_to :user
+      should_not_set_the_flash
+      should_render_template :edit
+      
+      should "display errors" do
+        assert_select "#errorExplanation"
+      end
+    end
+  end
 end
