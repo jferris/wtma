23f37c1ec56edfb135018b4df93d46e39b6f8e5e
Mike Burns <mburns@thoughtbot.com>
2008-10-19 09:55:39 -0400

    Fix the broken build

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -10,7 +10,13 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_match /<span class='dollar-sign'>\$<\/span>3.25/, number_to_formatted_currency(3.25)
   end
 
-  logged_in_user_context do
+  context "logged in" do
+    setup do
+      @user = Factory(:user)
+      @request.session[:user_id] = @user.id
+      ApplicationHelperTest.any_instance.stubs(:current_user).returns(@user)
+    end
+
     should "produce the name of the current user when sent #user_name" do
       assert_match @user.first_name, user_name
     end
