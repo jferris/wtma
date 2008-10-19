3d66faa99ae465ae866d5d02bd38d981882c9523
Mike Burns <mburns@thoughtbot.com>
2008-10-19 09:37:13 -0400

    The user's name in the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def number_to_formatted_currency(number)
     number_to_currency(number, :unit => "<span class='dollar-sign'>$</span>")
   end
+
+  def user_name
+    current_user.first_name
+  end
 end
--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -2,7 +2,17 @@ require File.dirname(__FILE__) + '/../../test_helper'
 require 'action_view/test_case'
 
 class ApplicationHelperTest < ActionView::TestCase 
+  def setup
+    @request = ActionController::TestRequest.new
+  end
+
   should "should wrap the dollar sign in a span" do
     assert_match /<span class='dollar-sign'>\$<\/span>3.25/, number_to_formatted_currency(3.25)
+  end
+
+  logged_in_user_context do
+    should "produce the name of the current user when sent #user_name" do
+      assert_match @user.first_name, user_name
+    end
   end
 end
