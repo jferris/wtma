f1b81249b1554d15a65d8f54afcad6fc4258fcef
Mike Burns <mburns@thoughtbot.com>
2008-10-18 20:01:30 -0400

    sessions/new uses logged-out layout. update the CSS.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
 class SessionsController < ApplicationController
   include Clearance::App::Controllers::SessionsController
+
+  layout 'logged_out', :only => [:new]
 end
--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,4 +2,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   include Clearance::Test::Functional::SessionsControllerTest
+
+  context "GET to new" do
+    setup do
+      get :new
+    end
+
+    should_render_with_layout 'logged_out'
+  end
 end
