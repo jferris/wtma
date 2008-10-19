e3892976fbe49eca40039647c039e877a345b41c
Mike Burns <mburns@thoughtbot.com>
2008-10-19 12:06:31 -0400

    root redirects to purchases_path if logged-in

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
+  before_filter :redirect_to_purchases, :if => :logged_in?
+
   def index
     @purchases = Purchase.cheapest_by_item
+  end
+
+  private
+
+  def redirect_to_purchases
+    redirect_to purchases_path
   end
 end
--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -3,35 +3,47 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ItemsControllerTest < ActionController::TestCase
   include ActionView::Helpers::AssetTagHelper
 
-  context "with some Purchases" do
-    setup do
-      cheap_purchase = Factory(:purchase, :price => 0.25)
-      item = cheap_purchase.item
-      expensive_purchase = Factory(:purchase, :price => 12.0, :item => item)
-    end
-
+  logged_in_user_context do
     context "GET to index" do
-      setup { get :index }
+      setup do
+        get :index
+      end
 
-      should_assign_to :purchases
-      should_render_with_layout 'logged_out'
+      should_redirect_to 'purchases_path'
+    end
+  end
 
-      should "set @purchases to the cheapest purchase for that item" do
-        assigns(:purchases).each do |purchase|
-          item = purchase.item
-          assert_equal item.cheapest_purchase, purchase
+  public_context do
+    context "with some Purchases" do
+      setup do
+        cheap_purchase = Factory(:purchase, :price => 0.25)
+        item = cheap_purchase.item
+        expensive_purchase = Factory(:purchase, :price => 12.0, :item => item)
+      end
+
+      context "GET to index" do
+        setup { get :index }
+
+        should_assign_to :purchases
+        should_render_with_layout 'logged_out'
+
+        should "set @purchases to the cheapest purchase for that item" do
+          assigns(:purchases).each do |purchase|
+            item = purchase.item
+            assert_equal item.cheapest_purchase, purchase
+          end
         end
-      end
-      
-      should_have_map
+        
+        should_have_map
 
-      should "center the map on the USA" do
-        assert_select "script", :text => /map.setCenter\(new GLatLng\(#{USA[:lat]}, #{USA[:long]}\), #{DEFAULT_ZOOM_LEVEL}\)/
-      end
+        should "center the map on the USA" do
+          assert_select "script", :text => /map.setCenter\(new GLatLng\(#{USA[:lat]}, #{USA[:long]}\), #{DEFAULT_ZOOM_LEVEL}\)/
+        end
 
-      should "put the cheapest Purchase for an Item on the map" do
-        assigns(:purchases).each do |purchase|
-          assert_select "script", :text => /GLatLng\(#{purchase.store.latitude},#{purchase.store.longitude}\)/
+        should "put the cheapest Purchase for an Item on the map" do
+          assigns(:purchases).each do |purchase|
+            assert_select "script", :text => /GLatLng\(#{purchase.store.latitude},#{purchase.store.longitude}\)/
+          end
         end
       end
     end
